7db78d50ff07bf3ba7811d5528c7c965c7380d3b
Matt Smithburger <smithburger.matthew@gmail.com>
2024-07-16 16:12:33 -0400

    fix: updating tests to work

--- jest.config.ts
+++ jest.config.ts
@@ -1,6 +1,8 @@
 import type { Config } from "jest";
 
 const config: Config = {
+  preset: "ts-jest",
+
   // Automatically clear mock calls, instances, contexts and results before every test
   clearMocks: true,
 
@@ -16,16 +18,32 @@ const config: Config = {
   // A list of paths to directories that Jest should use to search for files in
   roots: ["<rootDir>/src"],
 
+  // Map for ts config paths
+  moduleNameMapper: {
+    "@/(.*)$": "<rootDir>/src/$1",
+  },
+
+  // The test environment that will be used for testing
+  testEnvironment: "jsdom",
+
   // The glob patterns Jest uses to detect test files
   testMatch: ["**/__tests__/**/*.[jt]s?(x)", "**/?(*.)+(spec|test).[jt]s?(x)"],
 
-  // The test environment that will be used for testing
-  testEnvironment: "node",
+  // Ignore paths
+  coveragePathIgnorePatterns: [
+    "/node_modules/",
+    "/coverage",
+    "package.json",
+    "package-lock.json",
+  ],
 
   // A map from regular expressions to paths to transformers
   transform: {
     "^.+\\.tsx?$": "ts-jest",
   },
+
+  // Setup files after environment
+  setupFilesAfterEnv: ["<rootDir>/jest.setup.ts"],
 };
 
 export default config;
